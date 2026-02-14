f6e02746a386b93e17387a61d4ee07008437bf47
Eyal <eyalcourse@gmail.com>
2026-02-14 05:39:09 +0200

    feat(JWTvalidation): added key-pair consumption in auth.SecurityConfig

--- auth/src/main/java/com/budget/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/budget/auth/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.budget.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @Configuration
 public class SecurityConfig {
@@ -33,5 +38,26 @@ public class SecurityConfig {
             //Email notify NOC
             throw new RuntimeException("Critical error: SHA-256 alg not found");
         }
+    }
+
+    @Bean
+    public PrivateKey privateKey(@Value("${auth.private-key-path}") String path) throws Exception {
+        String key = new String(Files.readAllBytes(Paths.get(path)));
+        String privateKeyPEM = key
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replaceAll(System.lineSeparator(), "")
+                .replace("-----END PRIVATE KEY-----", "");
+
+        byte[] encoded = Base64.getDecoder().decode(privateKeyPEM);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+        return KeyFactory.getInstance("RSA").generatePrivate(keySpec);
+    }
+
+    @Bean
+    public PublicKey publicKey(@Value("${auth.public-key-str}") String publicKeyStr) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(publicKeyStr);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePublic(spec);
     }
 }
